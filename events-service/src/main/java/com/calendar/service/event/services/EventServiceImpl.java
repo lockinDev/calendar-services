d428ffddf43baf30e944c214c29f43546498ad35
lockinDev <javieralmario12@gmail.com>
2020-12-14 18:28:47 -0400

    Added Repository component to the EventServiceImpl class, for data access and management.

--- events-service/src/main/java/com/calendar/service/event/services/EventServiceImpl.java
+++ events-service/src/main/java/com/calendar/service/event/services/EventServiceImpl.java
@@ -2,6 +2,7 @@ package com.calendar.service.event.services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,31 +26,62 @@ public class EventServiceImpl implements EventService {
 
 	@Override
 	public Event getEvent(int eventId) {
-		 LOG.debug(String.format("getEvent: found entity : %s", eventId));
-		return new Event();
+		
+		Event event = eventRepository.findByEventId(eventId).get();
+
+		LOG.debug(String.format("getEvent: found entity : %s", eventId));
+		return event;
 	}
 
 	@Override
 	public Collection<Event> getEvents() {
-		 LOG.debug(String.format("getEvents: found entitys "));
-		return new ArrayList<Event>();
+		List<Event> events =  new ArrayList<Event>();		
+		eventRepository.findAll().forEach(events::add);;
+		
+		LOG.debug(String.format("getEvents: found entitys % s"), events.size());
+		return events;
 	}
 
 	@Override
 	public Event createEvent(Event body) {
-        LOG.debug(String.format("createEvent: entity created for : %s", body));
-        return new Event();
+		
+		try {
+            Event event = eventRepository.save(body);
+
+            LOG.debug(String.format("createEvent: entity created for : %s", body));
+            
+            return event;
+
+        } catch (Exception e) {
+        	LOG.debug(String.format("Exception: event for : %s", body));
+        	
+        }
+		
+		return null; 
 	}
 
 	@Override
 	public Event updateEvent(Event body) {
-		LOG.debug(String.format("updateEvent: entity update for : %s", body));
-        return new Event();
+		
+		try {
+            Event event = eventRepository.save(body);
+
+    		LOG.debug(String.format("updateEvent: entity update for : %s", body));
+            
+            return event;
+
+        } catch (Exception e) {
+        	LOG.debug(String.format("Exception: event for : %s", body));
+        	
+        }
+		
+		return null; 
 	}
 
 	@Override
 	public void deleteEvent(int eventId) {
         LOG.debug("deleteEvent: tries to delete an entity with eventId: {}", eventId);
+        eventRepository.findByEventId(eventId).ifPresent(e -> eventRepository.delete(e));
 	}
 	
 	
