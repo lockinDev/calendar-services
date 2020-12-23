cfeb1f890cd1b868a9718d06324ca5652ce4907e
lockinDev <javieralmario12@gmail.com>
2020-12-23 18:42:24 -0400

    Injected mongoTemplate for repository autoconfiguration, and event entity mapping.

--- events-service/src/main/java/com/calendar/service/event/EventsServiceApplication.java
+++ events-service/src/main/java/com/calendar/service/event/EventsServiceApplication.java
@@ -1,14 +1,45 @@
 package com.calendar.service.event;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.IndexResolver;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
+
+import com.calendar.service.model.Event;
 
 @SpringBootApplication
 public class EventsServiceApplication {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(EventsServiceApplication.class);
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventsServiceApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(EventsServiceApplication.class, args);
+	}
+	
+	@Autowired
+	MongoOperations mongoTemplate;
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void initIndicesAfterStartup() {
+
+		MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext = mongoTemplate.getConverter().getMappingContext();
+		IndexResolver resolver = new MongoPersistentEntityIndexResolver(mappingContext);
+
+		IndexOperations indexOps = mongoTemplate.indexOps(Event.class);
+		resolver.resolveIndexFor(Event.class).forEach(e -> indexOps.ensureIndex(e));
 	}
 
 }
