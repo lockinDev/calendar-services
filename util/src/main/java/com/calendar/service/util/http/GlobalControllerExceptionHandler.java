5ff0b7f51a6ea57635b041ba5d6378ce1bf32256
lockinDev <javieralmario12@gmail.com>
2021-01-02 21:41:40 -0400

    Added service for user entity management, data creation and retrieval.

--- util/src/main/java/com/calendar/service/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/com/calendar/service/util/http/GlobalControllerExceptionHandler.java
@@ -3,7 +3,6 @@ package com.calendar.service.util.http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
